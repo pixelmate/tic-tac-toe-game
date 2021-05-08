86708e5703cc1aff2bd1a6728650053ac8132681
pixelmate <prince.zlx9211@gmail.com>
2021-05-08 19:43:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+import Board from './components/Board';
+function App() {
+  return (
+    <div className='App'>
+      <div className='game'>
+        <h1>Tic Tac Toe</h1>
+        <Board />
+        {/* <div>game board</div> */}
+        <div className='Reset_button'>
+          <button className='btn btn-primary'>Reset</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
